1d2c3c15f4fb8b4d8e5acfa327dc766fa8348209
ikram <ikramtalbi906@gmail.com>
2023-10-26 22:47:46 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import MovieCard from './MovieCard';
+import MovieDescription from './MovieDescription';
+import moviesData from './MoviesData';
+
+
+
+import './App.css'; 
 
 function App() {
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Movies</h1>
+      <Routes>
+        <Route path="/" element={
+          <div className="movie-list">
+            {moviesData.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
+          </div>
+        } />
+        <Route path="movie/:movieId" element={<MovieDescription />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/MoviesData.js
@@ -0,0 +1,22 @@
+const moviesData = [
+    {
+      id: 1,
+      title: "La petit sérene",
+      posterURL: 'dis3.jpeg',
+      rating: 4.7,
+      description: "The plot follows the vigilante Batman, police lieutenant James Gordon, and district attorney Harvey Dent, who form an alliance to dismantle organized crime in Gotham City. Their efforts are derailed by the Joker, an anarchistic mastermind who seeks to test how far the Batman will go to save the city from chaos.",
+      trailer: "https://www.youtube.com/watch?v=UXmBT9JtuLo",
+    },
+    {
+      id: 2,
+      title: "Tiana",
+      posterURL: 'dis2.jpeg',
+      rating: 4.9,
+      description: "Two imprisoned men bond over several years.",
+      trailer: "https://www.youtube.com/watch?v=PLl99DlL6b4",
+    },
+    // Add more movies as needed
+  
+  ];
+  
+  export default moviesData;
